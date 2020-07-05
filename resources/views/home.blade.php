cc00b7f2edcadf88909145221613ecd2d513cc37
Dawid <januszakahipis@wp.pl>
2020-07-05 18:34:10 +0200

    view of home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img class="rounded-circle" style="height: 200px; width: 200px" src="https://upload.wikimedia.org/wikipedia/commons/d/de/Windows_live_square.JPG"/>
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Dawid Koziej</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"> <strong>Dawid</strong> </div>
+                <div class="pr-5"> <strong>Dawid</strong> </div>
+                <div class="pr-5"> <strong>Dawid</strong> </div>
             </div>
+            <div class="pt-5 font-weight-bold">Dawid Koziej</div>
+            <div>taesfasfsafafakoasmgnkalngajbgajgbalgnaslgbaslgbalgkbalsbgsajgbasljgbasljgbalgblasbsalbgalsbgalgbla</div>
+            <div><a href="google.com">google.com</a> </div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img class="w-100" src="https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.45.1200.1200a/s640x640/106145934_1126977754346770_8220164925137662043_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=H2j1UuunicgAX9nGf-a&oh=d41901aad77bfcab05d64f9d0a6c0e65&oe=5F2BE861"/>
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.45.1200.1200a/s640x640/106145934_1126977754346770_8220164925137662043_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=H2j1UuunicgAX9nGf-a&oh=d41901aad77bfcab05d64f9d0a6c0e65&oe=5F2BE861"/>
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.45.1200.1200a/s640x640/106145934_1126977754346770_8220164925137662043_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=H2j1UuunicgAX9nGf-a&oh=d41901aad77bfcab05d64f9d0a6c0e65&oe=5F2BE861"/>
         </div>
     </div>
 </div>
